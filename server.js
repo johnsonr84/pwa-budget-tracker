b9822479224b7799ee31ce34ff6f136506f56045
Your Name <robertcjohnson1984@gmail.com>
2021-08-23 01:08:13 -0600

    add to mongoose connect

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+//Set up PORT env or localhost
 const PORT = 3000;
 
 const app = express();
 
+//Initialize middelwares
 app.use(logger("dev"));
 
 app.use(compression());
@@ -15,9 +17,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//Connect to mongodb atlas and server
 mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
