c1f81968282eee5bc57ca26c20de0b475b85a837
Your Name <robertcjohnson1984@gmail.com>
2021-08-23 01:53:28 -0600

    install lifecycle method

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,3 +8,16 @@ const filesToCache = [
 	'/icons/icon-192x192.png',
 	'/icons/icon-512x512.png'
 ];
+
+const STATIC_CACHE = "static-cache-v2";
+const DATA_CACHE = "data-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+// Install lifecycle method
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(STATIC_CACHE)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
