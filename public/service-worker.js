93a4c40eb964914ca178adb9276d1281436ab337
Your Name <robertcjohnson1984@gmail.com>
2021-08-23 02:05:29 -0600

    add fetch logic

--- public/service-worker.js
+++ public/service-worker.js
@@ -36,4 +36,48 @@ self.addEventListener("activate", event => {
     );
   });
   
-  
+// fetch
+self.addEventListener("fetch", event => {
+    // non GET requests are not cached and requests to other origins are not cached
+    if (
+        event.request.method !== "GET" ||
+        !event.request.url.startsWith(self.location.origin)
+    ) {
+        event.respondWith(fetch(event.request));
+        return;
+    }
+
+    // handle runtime GET requests for data from /api routes
+    if (event.request.url.includes("/api/transaction")) {
+        // make network request and fallback to cache if network request fails (offline)
+        event.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request)
+                    .then(response => {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => caches.match(event.request));
+            })
+        );
+        return;
+    }
+
+    // use cache first for all other requests for performance
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            // request is not in cache. make network request and cache the response
+            return caches.open(RUNTIME_CACHE).then(cache => {
+                return fetch(event.request).then(response => {
+                    return cache.put(event.request, response.clone()).then(() => {
+                        return response;
+                    });
+                });
+            });
+        })
+    );
+});
