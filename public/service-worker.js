c4e2267cfc872920b082d7e78caf1a29de49c337
Your Name <robertcjohnson1984@gmail.com>
2021-08-23 01:55:33 -0600

    acti handler

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,3 +21,19 @@ self.addEventListener("install", event => {
       .then(self.skipWaiting())
   );
 });
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+        return Promise.all(cachesToDelete.map(cacheToDelete => {
+          return caches.delete(cacheToDelete);
+        }));
+      }).then(() => self.clients.claim())
+    );
+  });
+  
+  
